9e229fede6dae1338898253f923662a167ba0ecb
krane <christian.kranabetter@protonmail.com>
2025-09-09 23:24:12 +0200

    fix wg iptables rules for peer-to-peer communication

--- .wireguard/add-wg-iptables-rules.sh
+++ .wireguard/add-wg-iptables-rules.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
+# Allow incoming WireGuard UDP traffic on the public interface (eth0)
 iptables -A INPUT -i eth0 -p udp --dport 13231 -j ACCEPT
-iptables -A FORWARD -i wg0 -o eth0 -j ACCEPT
-iptables -A FORWARD -i eth0 -o wg0 -j ACCEPT
+# Allow all traffic from the WireGuard interface (wg0) to be forwarded
+iptables -A FORWARD -i wg0 -j ACCEPT
+# Allow traffic from the public interface (eth0) to be forwarded to the WireGuard interface (wg0)
+iptables -A FORWARD -o wg0 -j ACCEPT
+# Allow all incoming traffic on the WireGuard interface (wg0) to the server itself
 iptables -A INPUT -i wg0 -j ACCEPT
-
--- .wireguard/remove-wg-iptables-rules.sh
+++ .wireguard/remove-wg-iptables-rules.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 iptables -D INPUT -i wg0 -j ACCEPT
-iptables -D FORWARD -i eth0 -o wg0 -j ACCEPT
-iptables -D FORWARD -i wg0 -o eth0 -j ACCEPT
+iptables -D FORWARD -o wg0 -j ACCEPT
+iptables -D FORWARD -i wg0 -j ACCEPT
 iptables -D INPUT -i eth0 -p udp --dport 13231 -j ACCEPT
-
